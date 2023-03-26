a17e12f118775885f5a3f15a6848268e49bb53d1
OleaDíaz <bastian.olea@usach.cl>
2023-03-26 19:50:47 -0300

    Tdas
    4 TDAs nuevos

--- LabN°1_20403502_OleaDiaz.rkt
+++ LabN°1_20403502_OleaDiaz.rkt
@@ -1,9 +1,36 @@
 #lang racket
 ; Requerimientos funcionales 1
-; TDAs (Tipos de dato abstracto)
+; TDAs (Tipos de datos abstractos)
 
 
 (define system (lambda (name . data)
                  (if (string? name)
                      (list (list "01 03 23 12:00") name)
                      (list null))))
+
+(define drive(lambda(letter name capacity . data)
+               (if (and
+                   (char? letter)
+                   (string? name)
+                   (number? capacity))
+                   (list letter name capacity)
+                   (list null))))
+
+(define folder(lambda(name . data)
+                (if (string? name)
+                    (list name)
+                    (list null))))
+
+(define file(lambda(name type content size)
+              (if (and
+                  (string? name)
+                  (string? type)
+                  (string? content)
+                  (number? size))
+                  (list name type content size)
+                  (list null))))
+
+(define user(lambda(name)
+                (if (string? name)
+                    (list name)
+                    (list null))))
